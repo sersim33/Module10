68fc7eb15ede4462b2d17637707b6e2f97162fe4
sersim33 <sersim33@gmail.com>
2024-03-03 16:39:53 +0200

    com2

--- task12.py
+++ task12.py
@@ -19,5 +19,6 @@ class Contacts:
         self.contacts.append(contact)  
                 
 contacts = Contacts()  # Створення екземпляру класу Contacts
-contacts.add_contacts("Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True)  # Виклик методу add_contacts через екземпляр
+contacts.add_contacts("Wylie Pope", "(692) 802-2949", "est@utquamvel.net", True)
+contacts.add_contacts("ggeery Pope", "(888) 802-2949", "qst@hhgr.net", True)  # Виклик методу add_contacts через екземпляр
 print(contacts.list_contacts())         
